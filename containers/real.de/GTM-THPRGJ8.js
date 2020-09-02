151e13402f8a4c52241c08996eb8ab5c82e80321
dustinrecko <dustin.recko@gmail.com>
2020-09-02 19:00:06 +0200

    Auto Update

--- containers/real.de/GTM-THPRGJ8.js
+++ containers/real.de/GTM-THPRGJ8.js
@@ -1,6 +1,6 @@
 data = {
 "resource": {
-  "version":"616",
+  "version":"617",
   
   "macros":[{
       "function":"__e"
@@ -429,7 +429,7 @@ data = {
       "vtp_value":"50"
     },{
       "function":"__jsm",
-      "vtp_javascript":["template","(function(){try{var a=",["escape",["macro",94],8,16],";if(a){var e=parseInt(",["escape",["macro",104],8,16],"),b=0,c;for(c=0;c\u003Ca.length;c++){var f=a.charCodeAt(c);b=(b\u003C\u003C5)-b+f;b|=0}var g=Math.abs(b)%100;return g+1\u003E=e?1:0}}catch(d){a=\"undefined\"===typeof d||\"undefined\"===typeof d.toString()?\"Unknown error\":d.toString(),dataLayer.push({event:\"GAevent\",eventCategory:\"JavaScript Error\",eventAction:\"cs_shouldTrack\"+a})}})();"]
+      "vtp_javascript":["template","(function(){try{var a=",["escape",["macro",94],8,16],";if(a){var e=parseInt(",["escape",["macro",104],8,16],"),b=0,c;for(c=0;c\u003Ca.length;c++){var f=a.charCodeAt(c);b=(b\u003C\u003C5)-b+f;b|=0}var g=Math.abs(b)%100;return g\u003E=e?1:0}}catch(d){a=\"undefined\"===typeof d||\"undefined\"===typeof d.toString()?\"Unknown error\":d.toString(),dataLayer.push({event:\"GAevent\",eventCategory:\"JavaScript Error\",eventAction:\"cs_shouldTrack\"+a})}})();"]
     },{
       "function":"__c",
       "vtp_value":"UA-27218006-5"
@@ -756,6 +756,7 @@ data = {
       "vtp_enableProductReportingCheckbox":true,
       "vtp_enableNewCustomerReportingCheckbox":false,
       "vtp_enableEnhancedConversionsCheckbox":false,
+      "vtp_enableEnhancedConversionVariable":false,
       "vtp_enableRdpCheckbox":true,
       "vtp_enableTransportUrl":false,
       "tag_id":1
