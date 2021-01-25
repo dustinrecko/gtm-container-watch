406ff1ec5ad35977b24db8ed7a15ecd3214bf265
dustinrecko <dustin.recko@gmail.com>
2021-01-25 19:00:07 +0100

    Auto Update

--- containers/real.de/GTM-THPRGJ8.js
+++ containers/real.de/GTM-THPRGJ8.js
@@ -1,6 +1,6 @@
 data = {
 "resource": {
-  "version":"750",
+  "version":"751",
   
   "macros":[{
       "function":"__e"
@@ -107,7 +107,7 @@ data = {
       "vtp_javascript":["template","(function(){var a=\"object\"===typeof ",["escape",["macro",24],8,16],",b=\"pageView\"===",["escape",["macro",10],8,16],",c=\"GAevent\"===",["escape",["macro",10],8,16],"\u0026\u0026\"optimizely\"===",["escape",["macro",22],8,16],";if(a\u0026\u0026(b||c)){a=[];for(b=0;b\u003C",["escape",["macro",24],8,16],".length;b++)c=",["escape",["macro",24],8,16],"[b],\/([^\\.]+)\\.(null)?$\/.test(c)||a.push(c);return retVal=a.length?a.join(\"!\"):null}return null})();"]
     },{
       "function":"__jsm",
-      "vtp_javascript":["template","(function(){var a=",["escape",["macro",10],8,16],",b=\"optimizely;ab test;Error Messages;feature;csSessionKey;JavaScript Error;consent;cookieConsent\".split(\";\"),c=[\"view\",\"Payback\"];return\"bannerView\"==a?!0:\"GAevent\"!=a||-1===b.indexOf(",["escape",["macro",22],8,16],")\u0026\u0026-1===c.indexOf(",["escape",["macro",23],8,16],")?\"listView\"==a||\"listView_part2\"==a?!0:!1:!0})();"]
+      "vtp_javascript":["template","(function(){var a=",["escape",["macro",10],8,16],",b=\"optimizely;ab test;Error Messages;feature;csSessionKey;JavaScript Error;consent;cookieConsent\".split(\";\"),c=[\"view\",\"Payback\",\"review seen\"];return\"bannerView\"==a?!0:\"GAevent\"!=a||-1===b.indexOf(",["escape",["macro",22],8,16],")\u0026\u0026-1===c.indexOf(",["escape",["macro",23],8,16],")?\"listView\"==a||\"listView_part2\"==a?!0:!1:!0})();"]
     },{
       "function":"__v",
       "vtp_dataLayerVersion":2,
